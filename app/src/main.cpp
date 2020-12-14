3693ff6878868fb3969abd87c0f1c1f8d0a581d7
dirkarnez <smalldirkalex@gmail.com>
2020-12-15 01:57:09 +0800

    - wip fixing plain-crud

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -27,44 +27,23 @@ int main(int argc, char *argv[])
     /**/
 
   
-    list<int> data_list;
-    PlainCRUD<list<int>> plainCRUD;
-    plainCRUD.load(file_path, data_list);
+    //list<int> data_list;
+    Data data = {
+        0,
+        0
+    };
 
-    //data_list.push_back(123);
-    //plainCRUD.write(data_list);
+    PlainCRUD<Data> plainCRUD;
+    plainCRUD.load(file_path, data);
 
-    //plainCRUD.read(data_list);
+    cout << data.i << endl;
 
-    for (int n : data_list) {
-        cout << n << '\n';
-    }
 
-   //
+    //data.i = 12334;
+    //cout << data.i << endl;
 
-    //
+    //plainCRUD.write(data);
+
 
     system("pause");
-
-    /**/
-
-
-    //// Create a list containing integers
- 
- 
-    //// Add an integer to the front of the list
-    //l.push_front(25);
-    //// Add an integer to the back of the list
-    //l.push_back(13);
- 
-    //// Insert an integer before 16 by searching
-    //auto it = std::find(l.begin(), l.end(), 16);
-    //if (it != l.end()) {
-    //    l.insert(it, 42);
-    //}
- 
-    //// Iterate and print values of the list
-    //for (int n : l) {
-    //    std::cout << n << '\n';
-    //}
 }
--- plain-crud/include/plain-crud.h
+++ plain-crud/include/plain-crud.h
@@ -33,21 +33,26 @@ void PlainCRUD<DataType>::load(string file_path, DataType& data)
 template <typename DataType>
 void PlainCRUD<DataType>::read(DataType& data)
 {
+	//cout << this->file.tellg() << endl;
+	//this->file.seekg(0, ios::beg);
+	//cout << this->file.tellg() << endl;
+	//memset(&data, 0, sizeof(data));
+	std::cout << "before read, good=" << this->file.good() << ", bad=" << this->file.bad()
+		<< ", fail=" << this->file.fail() << ", eof=" << this->file.eof() << std::endl;
 
-		this->file.seekg(0, ios::beg);
-		//memset(&data, 0, sizeof(data));
-		this->file.read(reinterpret_cast<char*>(&data), sizeof(DataType));
-	
+	this->file.read(reinterpret_cast<const char*>(&data), sizeof(data));
 
+	std::cout << "after read, good=" << this->file.good() << ", bad=" << this->file.bad()
+		<< ", fail=" << this->file.fail() << ", eof=" << this->file.eof() << std::endl;
 }
 
 template <typename DataType>
 void PlainCRUD<DataType>::write(DataType& data)
 {
-	//this->file.seekp(0, ios::beg);
-	//this->file.write((char*)&data, sizeof(data));
+	cout << this->file.tellp() << endl;
 	this->file.seekp(0, ios::beg);
-	this->file.write(reinterpret_cast<const char*>(&data), sizeof(data));
+	cout << this->file.tellp() << endl;
+	this->file.write(reinterpret_cast<char*>(&data), sizeof(DataType));
 }
 
 template <typename DataType>
